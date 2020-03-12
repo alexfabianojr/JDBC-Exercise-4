30498c9c39ac63ae448907ebb3e999495cc50f41
alexfabianojr <alexfabianojr@gmail.com>
2020-03-12 00:27:35 -0300

    ---

--- src/application/App.java
+++ src/application/App.java
@@ -2,11 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 
